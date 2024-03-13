4b3821edf1e0115c1241a8d48d92b6c667cc4b90
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-03-13 11:05:21 +0000

    challenge 2

--- my-app/src/components/NavBarSimple.js
+++ my-app/src/components/NavBarSimple.js
@@ -1,30 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import css from "./css/NavBarSimple.module.css";
 
-
 class NavBarSimple extends Component {
+
     constructor(props) {
         super(props);
+        
         this.state = {
-            message: 'Welcome to My Gallery'
-        };
+            message: "Hello, guest!",
+            buttonText: "Log in"
+        }
     }
 
-    handleClick = () => {
-        const newMessage = this.state.message === 'Welcome to My Gallery' ? 'Goodbye!' : 'Welcome to My Gallery';
-        this.setState({ message: newMessage });
-        console.log(this.state.message);
+    handleclick = () => {
+        this.setState((prevState) => ({
+            message: prevState.message === "Hello, guest!" ? "Welcome back, user!" : "Hello, guest!",
+            buttonText: prevState.buttonText === "Log out" ? "Log in" : "Log out",
+        }), ()=> console.log(this.state.message))
+        
     }
 
     render() {
         return (
             <div className={css.NavBar}>
-                <h1>My Gallery Heading</h1>
-                <span>{this.state.message}</span>
-                <button onClick={this.handleClick}>{this.state.message === 'Welcome to My Gallery' ? 'Login' : 'Logout'}</button>
+                <h1>My Gallery</h1>
+                <div>
+                    <span>{this.state.message}</span>
+                    <button onClick={() => this.handleclick()}>{this.state.buttonText}</button>
+                </div>
             </div>
-        );
+        )
     }
 }
 
-export default NavBarSimple;
+export default NavBarSimple
+
